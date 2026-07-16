4fc68ea86777c71bcab1000f9ccceffc0da9f917
saurabhchaudhary-18 <saurabh1234singh123@gmail.com>
2026-07-16 23:15:44 +0530

    API added

--- src/components/Home/Team.js
+++ src/components/Home/Team.js
@@ -1,7 +1,7 @@
 const Team = [
-    { id: "team1", imageSource: "team-img/Modi.png", name: "Modiiii ji", job: "Astrolger" },
+    { id: "team1", imageSource: "team-img/Anushka.jpeg", name: "Anushka Bansal", job: "DataBase & API" },
     { id: "team2", imageSource: "team-img/Sandhya.jpeg", name: "Sandhya", job: "Backend Developer" },
-    { id: "team3", imageSource: "team-img/Saurabh.png", name: "Saurabh Singh", job: "Backend Developer" },
+    { id: "team3", imageSource: "team-img/Saurabh.png", name: "Saurabh Singh", job: "Backend Developer & Testing" },
     { id: "team4", imageSource: "team-img/Yatendra.png", name: "Yatendra Pratap Singh", job: "FrontEnd / UI Designer" },
 ]
 
